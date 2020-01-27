39da00adaa907991a74feb797f17d7378a3d70df
Vincent Mouriaux <vincent.mouriaux@le-campus-numerique.fr>
2020-01-27 09:52:17 +0100

    Maj characs et app pour faire le tour 1 de combat att + def avec 2 mages

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import characters as chars
 
-nom = input("Bienvenu Aventurier(e), quel sera votre nom ? ")
+nom1 = input("Bienvenu Player 1, quel sera votre nom ? ")
+nom2 = input("Bienvenu Player 2, quel sera votre nom ? ")
+player1 = chars.Wizard(nom1)
+player2 = chars.Wizard(nom2)
 
-héros = chars.Wizard(nom)
+print("Bienvenus dans l'arène, {} et {} vont maintenant s'affronter dans un combat sans merci !\n".format(player1, player2))
+
+equipped_weapon, attack_points = player1.attack()
+print("{} attaque {} avec {} et tente de lui infliger {} points de dégâts !\n".format(player1, player2, equipped_weapon, attack_points))
+
+defense_roll, healthp2 = player2.defend(equipped_weapon, attack_points)
+if defense_roll >= attack_points:
+    print("{} se défend avec {} également à hauteur de {}, ce qui lui permet de parer l'attaque de {} !".format(player2, equipped_weapon, defense_roll, player1))
+else:
+    print("{} se défend avec {} également à hauteur de {}, ce qui est insuffisant pour parer, ses PV tombent à {} sous les coups de {} !".format(player2,
+                                                                                                                equipped_weapon,
+                                                                                                                defense_roll, healthp2,
+                                                                                                                player1))
--- characters.py
+++ characters.py
@@ -15,28 +15,31 @@ class Wizard:
         return "{} the {}".format(self.name, self.__class__.__name__.lower())
 
     def attack(self):
-        magic_dice = randint(1, magic_dice_max)
-        sword_dice = randint(1, sword_dice_max)
-        bow_dice = randint(1, bow_dice_max)
+        magic_dice = randint(1, self.magic_dice_max)
+        sword_dice = randint(1, self.sword_dice_max)
+        bow_dice = randint(1, self.bow_dice_max)
 
         dices = sorted([
             ['magic', magic_dice],
             ['sword', sword_dice],
             ['bow', bow_dice]
         ], key=lambda x: x[1], reverse=True)
-        self.equipped_weapon = dices[0][0]
-        self.attack_points = dices[0][1]
+        equipped_weapon = dices[0][0]
+        attack_points = dices[0][1]
+        return equipped_weapon, attack_points
 
     def defend(self, weapon, attack_points):
         if weapon == 'magic':
-            defense_roll = randint(1, magic_dice_max)
+            defense_roll = randint(1, self.magic_dice_max)
         elif weapon == 'bow':
-            defense_roll = randint(1, bow_dice_max)
+            defense_roll = randint(1, self.bow_dice_max)
         else:
-            defense_roll = randint(1, sword_dice_max)
+            defense_roll = randint(1, self.sword_dice_max)
         if attack_points >= defense_roll:
-            health = health - (attack_points - defense_roll)
-            print("Vous n'avez pas réussi à parer, vos HP sont tombés à {}.".format(health))
-        else:
-            print("Attaque parée, aucun dommage subi.")
+            self.health = self.health - (attack_points - defense_roll)
+        return defense_roll, self.health
 
+"""
+hero = Wizard("Merlin")
+print(hero)
+"""
