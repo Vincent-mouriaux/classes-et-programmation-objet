d4559b039361fabe240031fd408dd3301c2f0f6f
Vincent Mouriaux <vincent.mouriaux@le-campus-numerique.fr>
2020-01-27 15:50:48 +0100

    maj bonus d'attaques calculés sur attributs privés + raise Exception

--- app.py
+++ app.py
@@ -11,42 +11,48 @@ def creation_personnage ():
         player = chars.Warrior(nom)
     return player
 
-player1 = creation_personnage()
-player2 = creation_personnage()
+def main():
 
-print("Bienvenus dans l'arène, {} et {} vont maintenant s'affronter dans un combat sans merci !\n".format(player1, player2))
-input("")
+    player1 = creation_personnage()
+    player2 = creation_personnage()
 
-while player1.health > 0 and player2.health > 0:
-    equipped_weapon, attack_points = player1.attack()
-    print("{} ({}pv) attaque {} ({}pv) avec {} et tente de lui infliger {} points de dégâts !".format(player1, player1.health, player2, player2.health, equipped_weapon, attack_points))
-
-    defense_roll, player2.health = player2.defend(equipped_weapon, attack_points)
-    if defense_roll >= attack_points:
-        print("{} se défend avec {} également à hauteur de {}, ce qui lui permet de parer l'attaque de {} !\n".format(player2, equipped_weapon, defense_roll, player1))
-    else:
-        print("{} se défend avec {} également à hauteur de {}, ce qui est insuffisant pour parer, ses PV tombent à {} sous les coups de {} !\n".format(
-            player2, equipped_weapon, defense_roll, player2.health, player1))
-
+    print("Bienvenus dans l'arène, {} et {} vont maintenant s'affronter dans un combat sans merci !\n".format(player1, player2))
     input("")
 
-    if player1.health > 0 and player2.health > 0:
-        equipped_weapon, attack_points = player2.attack()
-        print("{} ({}pv) attaque {} ({}pv) avec {} et tente de lui infliger {} points de dégâts !".format(player2, player2.health, player1, player1.health, equipped_weapon, attack_points))
+    while player1.health > 0 and player2.health > 0:
+        equipped_weapon, attack_points = player1.attack()
+        print("{} ({}pv) attaque {} ({}pv) avec {} et tente de lui infliger {} points de dégâts !".format(player1, player1.health, player2, player2.health, equipped_weapon, attack_points))
 
-        defense_roll, player1.health = player1.defend(equipped_weapon, attack_points)
+        defense_roll, player2.health = player2.defend(equipped_weapon, attack_points)
         if defense_roll >= attack_points:
-            print("{} se défend avec {} également à hauteur de {}, ce qui lui permet de parer l'attaque de {} !\n".format(player1, equipped_weapon, defense_roll, player2))
+            print("{} se défend avec {} également à hauteur de {}, ce qui lui permet de parer l'attaque de {} !\n".format(player2, equipped_weapon, defense_roll, player1))
         else:
             print("{} se défend avec {} également à hauteur de {}, ce qui est insuffisant pour parer, ses PV tombent à {} sous les coups de {} !\n".format(
-                player1, equipped_weapon, defense_roll, player1.health, player2))
+                player2, equipped_weapon, defense_roll, player2.health, player1))
 
         input("")
 
-if player1.health <= 0:
-    winner = player2
-    loser = player1
-else:
-    winner = player1
-    loser = player2
-print("Félicitations à {} qui a fait mordre la poussière à feu {} dans une démonstration de puissance indubitable ! ".format(winner, loser))
+        if player1.health > 0 and player2.health > 0:
+            equipped_weapon, attack_points = player2.attack()
+            print("{} ({}pv) attaque {} ({}pv) avec {} et tente de lui infliger {} points de dégâts !".format(player2, player2.health, player1, player1.health, equipped_weapon, attack_points))
+
+            defense_roll, player1.health = player1.defend(equipped_weapon, attack_points)
+            if defense_roll >= attack_points:
+                print("{} se défend avec {} également à hauteur de {}, ce qui lui permet de parer l'attaque de {} !\n".format(player1, equipped_weapon, defense_roll, player2))
+            else:
+                print("{} se défend avec {} également à hauteur de {}, ce qui est insuffisant pour parer, ses PV tombent à {} sous les coups de {} !\n".format(
+                    player1, equipped_weapon, defense_roll, player1.health, player2))
+
+            input("")
+
+    if player1.health <= 0:
+        winner = player2
+        loser = player1
+    else:
+        winner = player1
+        loser = player2
+    print("Félicitations à {} qui a fait mordre la poussière à feu {} dans une démonstration de puissance indubitable ! ".format(winner, loser))
+
+if __name__ == '__main__':
+    main()
+
--- characters.py
+++ characters.py
@@ -1,5 +1,7 @@
 from random import randint
 
+print(__name__)
+
 class Character:
 
     max_health = 12
@@ -10,6 +12,26 @@ class Character:
     def __init__(self, name):
         self.name = name
         self.health = self.max_health
+        self._height = randint(170, 190)
+        self._weight = randint(70, 90)
+
+    def get_height(self):
+        return self._height
+    def set_height(self, value):
+        if not isinstance(self._height, int):
+            raise Exception("Erreur de type sur la variable height !")
+        else:
+            self._height = value
+    height = property(get_height, set_height)
+
+    def get_weight(self):
+        return self._weight
+    def set_weight(self, value):
+        if not isinstance(self._weight, int):
+            raise Exception("Erreur de type sur la variable weight !")
+        else:
+            self._weight = value
+    weight = property(get_weight, set_weight)
 
     def __repr__(self):
         return "{} the {}".format(self.name, self.__class__.__name__.lower())
@@ -51,6 +73,10 @@ class Wizard(Character):
         if magic_dice > attack_points:
             equipped_weapon = 'magic'
             attack_points = magic_dice
+        elif equipped_weapon == 'sword':
+            attack_points = attack_points + (self.weight + self.height)//40
+        elif equipped_weapon == 'bow':
+            attack_points = attack_points + (self.height - 170) % 3
         return equipped_weapon, attack_points
 
 class Archer(Character):
@@ -61,8 +87,10 @@ class Archer(Character):
 
     def attack(self):
         equipped_weapon, attack_points = super().attack()
-        if equipped_weapon == 'magic' or equipped_weapon == 'sword':
-            attack_points = attack_points + 1
+        if equipped_weapon == 'sword':
+            attack_points = attack_points + self.height // 40 + 1
+        elif equipped_weapon == 'magic':
+            attack_points = attack_points + self.weight // 20 + 1
         return equipped_weapon, attack_points
 
 class Warrior(Character):
@@ -71,4 +99,12 @@ class Warrior(Character):
     magic_dice_max = 8
     bow_dice_max = 10
     sword_dice_max = 12
+
+    def attack(self):
+        equipped_weapon, attack_points = super().attack()
+        if equipped_weapon == 'magic':
+            attack_points = attack_points + self.weight // 30
+        elif equipped_weapon == 'bow':
+            attack_points = attack_points + (self.height - 170) % 3
+        return equipped_weapon, attack_points
 
